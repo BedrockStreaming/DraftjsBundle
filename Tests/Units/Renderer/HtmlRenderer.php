accddc3f9963c29da45d7ffefb9957f37a03a00e
yohann.poli <yohann.poli@gmail.com>
2017-03-14 22:48:33 +0100

    Fix AtomicBlockRenderer template from default to atomic

--- Tests/Units/Renderer/HtmlRenderer.php
+++ Tests/Units/Renderer/HtmlRenderer.php
@@ -14,11 +14,39 @@ class HtmlRenderer extends atoum
     use TestsContextTrait;
 
     /**
-     * Test exception undefined entityMap
+     * Test renderer with default block renderer
      */
-    public function testRender()
+    public function testRenderDefaultBlock()
     {
-        $json = '{"entityMap":{},"blocks":[{"key":"e0vbh","text":"Hello world!","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":2,"length":2,"style":"BOLD"}],"entityRanges":[],"data":{}}]}';
+        $json = <<<JSON
+{
+  "entityMap":{
+
+  },
+  "blocks":[
+    {
+      "key":"e0vbh",
+      "text":"Hello world!",
+      "type":"unstyled",
+      "depth":0,
+      "inlineStyleRanges":[
+        {
+          "offset":2,
+          "length":2,
+          "style":"BOLD"
+        }
+      ],
+      "entityRanges":[
+
+      ],
+      "data":{
+
+      }
+    }
+  ]
+}
+JSON;
+
         $rawState = $this->getRawState($json);
 
         $blockGuesser = $this->getMockBlockGuesser();
@@ -29,10 +57,65 @@ class HtmlRenderer extends atoum
             ->if($renderer = new TestedClass($converter, $builder))
             ->then
                 ->string($renderer->render($rawState))
-                ->isEqualTo('He<span class="bold">ll</span>o world!')
+                ->isEqualTo('<div class="default-block">He<span class="bold">ll</span>o world!</div>')
             ->then
                 ->mock($converter)->call('convertFromRaw')->withArguments($rawState)->once()
                 ->mock($builder)->call('build')->once()
+        ;
+    }
+
+    /**
+     * Test renderer with atomic block renderer
+     */
+    public function testRenderAtomicBlock()
+    {
+        $json = <<<JSON
+{
+    "entityMap":{
+        "0": {
+            "type":"ACME",
+            "mutability":"MUTABLE",
+            "data":{
+                "content": "This is me"
+            }
+        }
+    },
+    "blocks":[
+        {
+            "key":"7sf8s",
+            "text":" ",
+            "type":"atomic",
+            "depth":0,
+            "inlineStyleRanges":[
+            ],
+            "entityRanges":[
+                {
+                    "offset":0,
+                    "length":1,
+                    "key":"0"
+                }
+            ],
+            "data":{
+            }
+        }
+    ]
+}
+JSON;
+
+        $rawState = $this->getRawState($json);
+
+        $blockGuesser = $this->getMockBlockGuesser();
+        $converter = $this->getMockConverter();
+        $builder = $this->getMockBuilder($blockGuesser);
+
+        $this
+            ->if($renderer = new TestedClass($converter, $builder))
+                ->then
+                    ->string($renderer->render($rawState))
+                    ->isEqualTo('<figure class="atomic-block"><div class="acme-block-entity">This is me</div></figure>')
+                        ->then
+                            ->mock($converter)->call('convertFromRaw')->withArguments($rawState)->once()
+                            ->mock($builder)->call('build')->once()
         ;
     }
 }
